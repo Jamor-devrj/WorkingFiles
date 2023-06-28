17d4ff47f0fa5de7ed0acf90ac21f02a33c483dd
jamor <jamor.devbr@gmail.com>
2023-06-28 13:39:51 -0300

    FileWriter e BufferedWriter

---
+++ src/FileAndBuferedWriter/Program.java
@@ -0,0 +1,25 @@
+package FileAndBuferedWriter;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String[] lines = new String[] { "Good Morning", "Good Afternoon", "Good night"};
+		
+		String path = "C:\\workspace\\ws-eclipse\\working_with_files\\out.txt";
+		
+		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))){
+			for(String line : lines) {
+				bw.write(line);
+				bw.newLine();
+			}
+		}
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
